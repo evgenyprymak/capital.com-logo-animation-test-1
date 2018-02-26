2c0731ba6d53adf4d13b23440ed1846c9a2314df
E <prymakev@gmail.com>
2018-02-26 17:45:44 +0300

    2

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,21 @@
-var animation = bodymovin.loadAnimation({
+var animData_logo = {
   container: document.getElementById('capital-logo'),
   renderer: 'svg',
-  loop: true,
+  loop: false,
   autoplay: true,
   path: 'data.json'
-})
+}
+
+
 var animation_demo = bodymovin.loadAnimation({
   container: document.getElementById('demo'),
   renderer: 'svg',
-  loop: true,
+  loop: false,
   autoplay: true,
   path: 'data2.json'
 })
+
+
+var anim_logo = bodymovin.loadAnimation(animData_logo);
+
+anim_logo.frameRate = 2;
