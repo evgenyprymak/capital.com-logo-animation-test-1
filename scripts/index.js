6d3ba989acc522936770b5acadebe5c55d78081d
E <prymakev@gmail.com>
2018-02-27 10:13:35 +0300

    2

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,7 @@ var animData_demo ={
   container: document.getElementById('demo'),
   renderer: 'svg',
   loop: false,
-  autoplay: true,
+  autoplay: false,
   path: 'data2.json'
 }
 
@@ -18,8 +18,16 @@ var anim_logo = bodymovin.loadAnimation(animData_logo);
 var anim_demo = bodymovin.loadAnimation(animData_demo);
 
 
-anim_logo.addEventListener("click", myFunction);
 
-function myFunction() {
-    alert ("Hello World!");
+
+
+anim_logo.addEventListener("enterFrame", f_demo_delay);
+
+var v_demo_delay = 500;
+
+function f_demo_delay() {
+    timeoutId = setTimeout(f_demo_go, v_demo_delay)
+}
+function f_demo_go() {
+  anim_demo.play();
 }
